a415f64e48c23df11ec9ec9126744374136c38c3
Ahadul360 <ahadul360@gmail.com>
2023-01-18 16:54:11 +0600

    V4

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,7 +1,17 @@
 
 
 let menu = document.querySelector('.menu-box i');
-let nav = document.querySelector('.bottom-nav').classList;
+let nav = document.querySelector('.bottom-nav');
+let contentOverlay = document.querySelector('.content-overlay');
 menu.addEventListener('click',()=>{
-    nav.toggle('toggle-nav');
-})
+    nav.classList.toggle('toggle-nav');
+    contentOverlay.classList.toggle('active-overlay');
+});
+
+let w = document.documentElement.clientWidth;
+  if (w <= 768) {
+    contentOverlay.onclick=()=>{
+        nav.classList.toggle('toggle-nav');
+        contentOverlay.classList.toggle('active-overlay');
+    }
+  }

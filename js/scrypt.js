3072e2fe9ab55a9acaace21d34cc1925e491fb55
Ahadul360 <ahadul360@gmail.com>
2023-01-24 21:03:54 +0600

    Latest Update 10.0

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,17 +1,18 @@
 
 
-let menu = document.querySelector('.menu-box i');
-let nav = document.querySelector('.bottom-nav');
-let contentOverlay = document.querySelector('.content-overlay');
+let menu = document.querySelector('.menu-box i')
+let nav = document.querySelector('.bottom-nav')
+let contentOverlay = document.querySelector('.content-overlay')
 menu.addEventListener('click',()=>{
-    nav.classList.toggle('toggle-nav');
-    contentOverlay.classList.toggle('active-overlay');
+    nav.classList.toggle('toggle-nav')
+    contentOverlay.classList.toggle('active-overlay')
 });
 
-let w = document.documentElement.clientWidth;
-  if (w <= 768) {
-    contentOverlay.onclick=()=>{
-        nav.classList.toggle('toggle-nav');
-        contentOverlay.classList.toggle('active-overlay');
-    }
+
+let w = document.documentElement.clientWidth
+if (w <= 768) {
+  contentOverlay.onclick=()=>{
+      nav.classList.toggle('toggle-nav')
+      contentOverlay.classList.toggle('active-overlay')
   }
+}

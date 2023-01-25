de441e627da5c62a0f537e47230db093ec698ca9
Ahadul360 <ahadul360@gmail.com>
2023-01-25 19:49:21 +0600

    Final Version

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,31 +1,24 @@
-
-
 let menu = document.querySelector('.menu-box i')
 let nav = document.querySelector('.bottom-nav')
 let contentOverlay = document.querySelector('.content-overlay')
+
 menu.addEventListener('click',()=>{
-    nav.classList.toggle('toggle-nav')
-    contentOverlay.classList.toggle('active-overlay')
-    document.body.classList.toggle('no-scroll')
+  nav.classList.toggle('active-nav')
+  contentOverlay.classList.toggle('active-overlay')
+  document.body.classList.toggle('no-scroll')
 });
 
-
-let w = document.documentElement.clientWidth
-if (w <= 768) {
-  contentOverlay.onclick=()=>{
-      nav.classList.toggle('toggle-nav')
-      contentOverlay.classList.toggle('active-overlay')
-      document.body.classList.toggle('no-scroll')
-  }
+contentOverlay.onclick=()=>{
+  nav.classList.toggle('active-nav')
+  contentOverlay.classList.toggle('active-overlay')
+  document.body.classList.toggle('no-scroll')
 }
 
 window.addEventListener("resize", () => {
   let cw = document.documentElement.clientWidth;
-  if (cw <= 768) {
-    contentOverlay.onclick=()=>{
-      nav.classList.toggle('toggle-nav')
-      contentOverlay.classList.toggle('active-overlay')
-      document.body.classList.toggle('no-scroll')
-    }
+ if(cw > 768){
+    document.body.classList.remove('no-scroll')
+    contentOverlay.classList.remove('active-overlay')
+    nav.classList.remove('active-nav')
   }
 });

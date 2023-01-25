2c8b4b618014a1e8f6eff4214e586e2c8562b104
Ahadul360 <ahadul360@gmail.com>
2023-01-26 02:04:44 +0600

    V21

--- js/scrypt.js
+++ js/scrypt.js
@@ -19,11 +19,11 @@ searchIcon.addEventListener('click',()=>{
   document.querySelector('.search-box form').classList.toggle('show-search-box')
 })
 
-window.addEventListener("resize", () => {
-  let cw = document.documentElement.clientWidth;
- if(cw > 768){
-    document.body.classList.remove('no-scroll')
-    contentOverlay.classList.remove('active-overlay')
-    nav.classList.remove('active-nav')
-  }
-});
+// window.addEventListener("resize", () => {
+//   let cw = document.documentElement.clientWidth;
+//  if(cw > 768){
+//     document.body.classList.remove('no-scroll')
+//     contentOverlay.classList.remove('active-overlay')
+//     nav.classList.remove('active-nav')
+//   }
+// });
